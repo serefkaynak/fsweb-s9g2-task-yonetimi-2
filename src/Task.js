2958477e3938b77e20b6b30f51315715f6027797
Şeref Kaynak <serefkynk@gmail.com>
2023-12-09 13:19:54 +0300

    first

--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { differenceInDays, formatDistance, subDays } from 'date-fns';
+
 
 const Task = ({ taskObj, onComplete }) => {
 
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline">Son Teslim: 
+      <span>
+        {differenceInDays(new Date(taskObj.deadline), new Date())} gün sonra
+      </span></div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
